768a242f9caaf336e534b3f7abefef8cd937d3fd
h我叫小刀 <214564382@qq.com>
2017-06-16 11:56:59 +0800

    修改CI框架使其默认控制器支持子目录

--- App/config/routes.php
+++ App/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  |		my-controller/my-method	-> my_controller/my_method
  */
  
-$route['default_controller'] = 'Home/home/index';
+$route['default_controller'] = 'Home/home/test';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 //$roote['Home'] = 'Home/Home';
