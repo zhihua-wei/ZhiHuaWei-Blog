121db7fd724903c566d87c0cc94b632e7f69fec3
h我叫小刀 <214564382@qq.com>
2017-06-16 11:59:36 +0800

    修改CI框架使其默认控制器支持子目录

--- App/controllers/Home/Home.php
+++ App/controllers/Home/Home.php
@@ -3,11 +3,22 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
-    public function index() {
+    public function index()
+    {
 
         echo 1111111;
+
+        //$this -> load -> view('welcome.html');
+
+    }
+
+    public function test()
+    {
+
+        echo 11115555111;
 
         //$this -> load -> view('welcome.html');
 
