56293cbb7119b32c18f008fe9b0894d5f2d0f6a8
h我叫小刀 <214564382@qq.com>
2017-06-16 15:40:46 +0800

    文件整理

--- App/config/routes.php
+++ App/config/routes.php
@@ -50,6 +50,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  |		my-controller/my-method	-> my_controller/my_method
  */
  
-$route['default_controller'] = 'Home/home/test';
+$route['default_controller'] = 'Weiadmin/Login/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
