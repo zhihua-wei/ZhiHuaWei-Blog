4c7359f6624c630e318aaf8bd382a43babeb7732
h我叫小刀 <214564382@qq.com>
2017-06-16 16:17:39 +0800

    开发后台登陆功能模块

--- App/models/setting_model.php
+++ App/models/setting_model.php
@@ -16,7 +16,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Setting_model extends CI_Model {
 	const TBL_DEVLOG = 'devlog';
-	const TBL_SETTING = 'setting';
+	const TBL_SETTING = 'administrator';
 	const TBL_AUTHRULE = 'auth_rule';
 
 	/**
