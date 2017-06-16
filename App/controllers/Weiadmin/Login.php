b2a84827bc9b802834acbeaa0b90ab3966d8afbb
h我叫小刀 <214564382@qq.com>
2017-06-16 15:49:17 +0800

    文件整理

--- App/controllers/Weiadmin/Login.php
+++ App/controllers/Weiadmin/Login.php
@@ -5,4 +5,8 @@
  * Author: ZhiHua_Wei <zhihua_wei@foxmail.com>
  * Date: 2017/6/16
  * Time: 15:42
+ * Project: ZhiHuaWei-Blog 系统
+ * Version: 1.0.0
+ * Power:  后台登录控制器
+ * ====================================================
  */
