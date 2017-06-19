01a0b3f5dc6e1262772b01061fb402c2572354d8
h我叫小刀 <214564382@qq.com>
2017-06-19 11:37:35 +0800

    后台博客设置控制器

--- App/controllers/Weiadmin/Blogset.php
+++ App/controllers/Weiadmin/Blogset.php
@@ -10,3 +10,48 @@
  * Power:  后台博客设置控制器
  * ====================================================
  */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Blogset extends Wei_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('login_model', 'admin');
+        $this->load->library('pagination');
+    }
+
+    /**
+     * 轮播图设置
+     */
+    public function carousel()
+    {
+    }
+
+
+    public function index($offset = '')
+    {
+        $data = $this->weiData;
+        //删除三十天前的日志记录
+        $t = time() - 3600 * 24 * 30;
+        $this->admin->del_month_ago_log($t);
+        //配置分页信息
+        $config['base_url'] = site_url('Weiadmin/Admin/index/');
+        $config['total_rows'] = $this->admin->get_log_count($this->ADMINISTRSTORS['admin_id']);
+        $config['per_page'] = 10;
+        //初始化分类页
+        $this->pagination->initialize($config);
+        //生成分页信息
+        $data['pageinfo'] = $this->pagination->create_links();
+        $data['admin_log'] = $this->admin->get_admin_log_list($this->ADMINISTRSTORS['admin_id'], $config['per_page'], $offset);
+        $this->load->view('admin.html', $data);
+    }
+
+    /**
+     * 背景图设置
+     */
+    public function background()
+    {
+    }
+}
