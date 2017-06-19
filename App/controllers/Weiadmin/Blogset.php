5b00b1e73541a4c21a189440eaa6b3b30d62ec44
h我叫小刀 <214564382@qq.com>
2017-06-19 16:01:28 +0800

    整理博客配置页面

--- App/controllers/Weiadmin/Blogset.php
+++ App/controllers/Weiadmin/Blogset.php
@@ -18,20 +18,34 @@ class Blogset extends Wei_Controller
     public function __construct()
     {
         parent::__construct();
-        //$this->load->model('config_model', 'config');
+        $this->load->model('config_model', 'con');
         $this->load->library('pagination');
     }
 
     /**
      * 博客设置
      */
-    public function setting()
+    public function config()
     {
         $data = $this->weiData;
         $data['config'] = $this->wei->get_config();
-        var_dump($data['config']);
-        exit;
-        $this->load->view('config.html', $data);
+        if ($_POST) {
+            $params['title'] = $this->input->post('title');
+            $params['sitename'] = $this->input->post('sitename');
+            $params['keywords'] = $this->input->post('keywords');
+            $params['footer'] = $this->input->post('footer');
+            $params['description'] = $this->input->post('description');
+            $this->con->update_site_setting($params);
+            $this->wei->add_log('修改网站配置信息！', $this->ADMINISTRSTORS['admin_id'], $this->ADMINISTRSTORS['username']);
+            $success['msg'] = "网站信息设置成功！";
+            $success['url'] = site_url("Weiadmin/Blogset/config");
+            $success['wait'] = 3;
+            $data['success'] = $success;
+            $this->load->view('success.html', $data);
+        } else {
+            $this->load->view('config.html', $data);
+        }
+        //$this->load->view('config.html', $data);
     }
 
     /**
