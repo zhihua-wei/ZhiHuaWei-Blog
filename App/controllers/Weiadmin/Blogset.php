e44bffba1510c2346091c8445e41ac155072e7da
h我叫小刀 <214564382@qq.com>
2017-06-22 08:34:38 +0800

    博客轮播图功能模块开发

--- App/controllers/Weiadmin/Blogset.php
+++ App/controllers/Weiadmin/Blogset.php
@@ -267,24 +267,4 @@ class Blogset extends Wei_Controller
         }
     }
 
-
-    public function index($offset = '')
-    {
-        $data = $this->weiData;
-        //删除三十天前的日志记录
-        $t = time() - 3600 * 24 * 30;
-        $this->admin->del_month_ago_log($t);
-        //配置分页信息
-        $config['base_url'] = site_url('Weiadmin/Admin/index/');
-        $config['total_rows'] = $this->admin->get_log_count($this->ADMINISTRSTORS['admin_id']);
-        $config['per_page'] = 10;
-        //初始化分类页
-        $this->pagination->initialize($config);
-        //生成分页信息
-        $data['pageinfo'] = $this->pagination->create_links();
-        $data['admin_log'] = $this->admin->get_admin_log_list($this->ADMINISTRSTORS['admin_id'], $config['per_page'], $offset);
-        $this->load->view('admin.html', $data);
-    }
-
-
 }
