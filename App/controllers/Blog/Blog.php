ddc7740f3f793c7b74d3fb5abbe20c2019632d8c
h我叫小刀 <214564382@qq.com>
2017-06-24 16:17:33 +0800

    博客首页搭建

---
+++ App/controllers/Blog/Blog.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * ====================================================
+ * Created by ZHI HUA WEI.
+ * Author: ZhiHua_Wei <zhihua_wei@foxmail.com>
+ * Date: 2017/6/24
+ * Time: 16:16
+ * Project: ZhiHuaWei-Blog 系统
+ * Version: 1.0.0
+ * Power:  前台博客控制器
+ * ====================================================
+ */
--- App/controllers/Blog/Home.php
+++ App/controllers/Blog/Home.php
@@ -1,6 +1,16 @@
 <?php
 
-
+/**
+ * ====================================================
+ * Created by ZHI HUA WEI.
+ * Author: ZhiHua_Wei <zhihua_wei@foxmail.com>
+ * Date: 2017/6/24
+ * Time: 16:16
+ * Project: ZhiHuaWei-Blog 系统
+ * Version: 1.0.0
+ * Power:  前台首页控制器
+ * ====================================================
+ */
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends Home_Controller
@@ -9,6 +19,7 @@ class Home extends Home_Controller
     public function index()
     {
         $data = $this->homeData;
+        //记录访问人信息，地址，ip，日期，访问量+1
 
         $this->load->view('index.html', $data);
     }
