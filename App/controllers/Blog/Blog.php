21007db688933c77607df62004140f1c20ee68cb
h我叫小刀 <214564382@qq.com>
2017-06-27 16:58:38 +0800

    博客页

--- App/controllers/Blog/Blog.php
+++ App/controllers/Blog/Blog.php
@@ -34,7 +34,6 @@ class Blog extends Home_Controller
         $config['base_url'] = site_url('Blog/Blog/index/') . $id . "/";
         $config['total_rows'] = $this->blog->get_blog_count($id);
         $config['per_page'] = 10;
-
         var_dump($config);
         exit;
 
