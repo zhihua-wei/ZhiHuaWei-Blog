bda71f158549f28a075bb38847c3ad1b5b698844
h我叫小刀 <214564382@qq.com>
2017-06-27 17:17:00 +0800

    博客页

--- App/models/blog_model.php
+++ App/models/blog_model.php
@@ -34,10 +34,10 @@ class Blog_model extends CI_Model
 
     /**
      * 函数：获取博客列表
-     * @param int $admin_id 管理员id
+     * @param int $category_id 分类id
      * @param int $limit 每页显示数
      * @param int $offset 偏移量
-     * @return array 操作日志列表
+     * @return array 博客列表列表
      */
     public function get_blog_list($category_id = null, $limit, $offset)
     {
