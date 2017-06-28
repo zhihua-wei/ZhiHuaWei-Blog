ef60da0dd8c4c8009ad8c5e3d5b3d857e3acb3e0
h我叫小刀 <214564382@qq.com>
2017-06-28 17:22:43 +0800

    博客页

--- App/controllers/Blog/Blog.php
+++ App/controllers/Blog/Blog.php
@@ -30,6 +30,10 @@ class Blog extends Home_Controller
 
     public function index($id, $offset = '')
     {
+        var_dump($id);
+        var_dump($offset);
+        exit;
+
         $data = $this->homeData;
         //配置分页信息
         $config['base_url'] = site_url('Blog/Blog/index/') . $id . "/";
