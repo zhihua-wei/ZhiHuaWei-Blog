868a01ceac9b221f21767b6ee406a86de1bab01e
h我叫小刀 <214564382@qq.com>
2017-06-28 17:15:45 +0800

    博客页

--- App/controllers/Blog/Blog.php
+++ App/controllers/Blog/Blog.php
@@ -32,7 +32,7 @@ class Blog extends Home_Controller
         $data = $this->homeData;
         //配置分页信息
         $config['base_url'] = site_url('Blog/Blog/index/') . $id . "/";
-        $config['total_rows'] = $this->blog->get_blog_count();
+        $config['total_rows'] = $this->blog->get_blog_count($id);
         $config['per_page'] = 10;
 
         //初始化分类页
