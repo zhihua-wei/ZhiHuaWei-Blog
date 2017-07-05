fd8e11e24bdb797ab135194dc82f04d22635ea54
h我叫小刀 <214564382@qq.com>
2017-07-05 17:45:21 +0800

    博客文章分页

--- App/controllers/Blog/Blog.php
+++ App/controllers/Blog/Blog.php
@@ -28,6 +28,7 @@ class Blog extends Home_Controller
 
 
 
+
         //配置分页信息
         $config['base_url'] = site_url("Blog/Blog/index/");
         $config['total_rows'] = $this->admin->get_log_count(1);
