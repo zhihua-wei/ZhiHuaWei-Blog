836bd0f05f780c77fbf9df92bab3d2fd693e4d19
h我叫小刀 <214564382@qq.com>
2017-10-08 14:36:59 +0800

    博客文章分页

--- index.php
+++ index.php
@@ -188,7 +188,7 @@ if (defined('STDIN')) {
 	chdir(dirname(__FILE__));
 }
 
-echo date("Y-m-d H:i:s", strtotime("-1 year"));
+echo date("Y-m-d H:i:s", strtotime("1 year"));
 exit;
 
 if (($_temp = realpath($system_path)) !== FALSE) {
